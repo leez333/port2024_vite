0f0fd89da12242511d9f74c7146b3a200b9acb74
leez333 <leunji26@gmail.com>
2024-05-11 01:32:15 +0900

    헤더

--- port2024_vite/src/assets/js/menu.js
+++ port2024_vite/src/assets/js/menu.js
@@ -1,3 +1,24 @@
 export function menu() {
-    console.log(menu);
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
+    
+    if(headerToggle){
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+
+            headerToggle.getAttribute("aria-expanded") === "true" 
+            ? headerToggle.setAttribute("aria-expanded", "false") 
+            : headerToggle.setAttribute("aria-expanded", "true");/* 아래 if문과 동일 */
+        });
+    }    
+
+        // if(headerToggle.getAttribute("aria-expanded") === "true"){
+        //     headerToggle.setAttribute("aria-expanded", "false")
+        // } else {
+        //     headerToggle.setAttribute("aria-expanded", "true");
+        // }
+
+
+
+    
 };
